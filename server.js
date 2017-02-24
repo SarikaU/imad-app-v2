07e67056c77b1f3c705d1cb3d0f8b7e37fa51865
SarikaU <sarikau1986@gmail.com>
2017-02-24 23:31:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-1', function (req, res) {
-  res.sendFile('article 1 requested and will be served here');
+  res.send('article 1 requested and will be served here');
 });
 
 app.get('/article-2', function (req, res) {
-  res.sendFile('article 2 requested and will be served here');
+  res.send('article 2 requested and will be served here');
 });
 
 app.get('/article-3', function (req, res) {
-  res.sendFile('article 3 requested and will be served here');
+  res.send('article 3 requested and will be served here');
 });
 
 
