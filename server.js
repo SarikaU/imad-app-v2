6a78df01663afe208d37fca49af77a213795cf1d
SarikaU <sarikau1986@gmail.com>
2017-03-02 20:58:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,6 +115,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));  //to avoid the 404 not found error
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
