6cd210cf56811ce8c7a161bd3a09c12f25037826
SarikaU <sarikau1986@gmail.com>
2017-03-04 16:17:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,40 @@
 //counter button code
 
-var button=document.getElementById('counter');
-var counter=0;
-button.onclick=function(){
-    // Make a request to the counter endpoint
-    //Capture the response and store it in a variable
- 
- 
-    //Render the variable in the correct span
+ var button=document.getElementById('counter');
+ button.onclick=function(){
     
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-}
+    
+    // create a request object
+    
+ var request=new XMLHttprequest();
+    
+    
+    //Capture the response and store it in a variable
+    
+ request.onreadystatechange = function(){            //if request state changes then execute this function
+ 
+     if (request.readyState === XMLHttpRequest.DONE){  //checks the req current state & if its XMLHttpRequest.Done(req successfully completed,                                                we want to take some action .If req not done yet,then ignore)
+     
+          if(request.status ===200){                  //if req status is 200,req is successfully completed
+          
+                   var counter= request.responseText;          //gives access to the response value
+                   var span=document.getElementById('count');
+                   span.innerHTML=counter.toString();
+          }
+
+     }
+     
+ }; 
+  
+  
+      //make a request
+
+request.open('GET','http://sarikau.imad.hasura-app.io/counter',true);
+request.send(null)
+      
+     
+ };     
+
+    
+    
+    
