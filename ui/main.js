8c25f993d673b8d6b96f47b7f2887d0bcc1185c3
SarikaU <sarikau1986@gmail.com>
2017-03-04 15:38:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,15 @@
-console.log('Loaded!');
+//counter button code
 
-// to change the text of main-text div
-
-var element = document.getElementById('main-text');
-element.innerHTML = 'Welcome to the WebApp';
-
-//to make the image madi move when clicked
-
-var img = document.getElementById('madi');
-var marginLeft=0;
-
-function moveRight(){
-    marginLeft=marginLeft+5;
-    img.style.marginLeft=marginLeft+'px';
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function(){
+    // Make a request to the counter endpoint
+    //Capture the response and store it in a variable
+ 
+ 
+    //Render the variable in the correct span
     
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 }
-
-img.onclick = function(){
-    var interval=setInterval(moveRight,50); // to move madi gradually,interval is set
-  
-};
