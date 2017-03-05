0caf8f203eabe3f2d56ad2f9b27a61533138af38
SarikaU <sarikau1986@gmail.com>
2017-03-05 23:06:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -48,13 +48,39 @@ request.send(null);
  submit.onclick= function() {
  //make a request to the server and send the name
  //capture the list of names and render it as a list
- var names =['name1','name2','name3','name4'];
- var list='';
- for(var i=0; i< names.length; i++){
-     list += '<li>'+names[i]+'</li>';
- }
+ 
+ 
+  var request = new XMLHttpRequest();    //Capture the response and store it in a variable
+    
+ request.onreadystatechange = function(){       
+ 
+     if (request.readyState === XMLHttpRequest.DONE){  
+     
+          if(request.status ===200){   
+          
+                   var names= request.responseText;         
+                   names =JSON.parse(names);
+                   var list='';
+                   
+                   for(var i=0; i< names.length; i++){
+                       
+                        list += '<li>'+names[i]+'</li>';
+                    }
+                    
  var ul =document.getElementById('namelist');
  ul.innerHTML=list;
+ 
+
+     }
+     
+     }
+ }; 
+  
+   //make a request
+
+request.open('GET','http://sarikau.imad.hasura-app.io/submit-name?name=' + name,true);
+request.send(null);
+     
  
  };
  
