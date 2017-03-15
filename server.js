a393d9fabc3ee134e872c4ce897da7f787270487
SarikaU <sarikau1986@gmail.com>
2017-03-15 16:27:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ app.get('/submit-name', function (req, res) {  //submit-name?name=xxxxx
 
 app.get('/articles/:articleName', function (req, res) {
    
-    pool.query("SELECT * FROM article WHERE title=" +req.params.articleNam,function(err,result){
+    pool.query("SELECT * FROM article WHERE title='" +req.params.articleNam+"'",function(err,result){
         if (err){
             res.status(500).send(err.toString());
         }
